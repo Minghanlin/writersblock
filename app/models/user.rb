0e34908c25286b70249a27b4a33413e3f695cd3b
Minghan Lin <cd>
2016-09-12 23:44:07 +0800

    signup success

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,22 @@
 class User < ApplicationRecord
   before_save { email.downcase! }
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name,  presence: true, length: { maximum: 50, message: 'Ridiculously long name' }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence:   true, length: { maximum: 255 },
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+
+
+  private
+
+  # Validates the size of an uploaded picture.
+    def picture_size
+        if picture.size > 5.megabytes
+        errors.add(:picture, "picture should be less than 5MB")
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root 'static_pages#home'
 
@@ -8,6 +7,10 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
 
   get  '/signup',  to: 'users#new'
+
+  post '/signup',  to: 'users#create'
+
+  resources :users
 
 
 end
